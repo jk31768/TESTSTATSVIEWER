b3a90b58e5eb3ad71b98cccafda3783a234bb262
Juna Kim <31077956+jk31768@users.noreply.github.com>
2021-03-26 16:52:48 -0400

    Update __init__.py

--- StatsDataViewer/__init__.py
+++ StatsDataViewer/__init__.py
@@ -1,3 +1,7 @@
+import os 
+
+REFRESH_LOGO = os.path.abspath(os.path.join(os.path.dirname(__file__), 'glue_refresh.png'))
+
 def setup():
     from .StatsDataViewer import StatsDataViewer
     from glue.config import qt_client
