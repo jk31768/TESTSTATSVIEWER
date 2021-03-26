585361f92d69d8d9f6c8256cbd8e292d672d4ec0
Juna Kim <31077956+jk31768@users.noreply.github.com>
2021-03-26 17:02:50 -0400

    Update __init__.py

--- StatsDataViewer/__init__.py
+++ StatsDataViewer/__init__.py
@@ -1,6 +1,6 @@
-#import os 
+import os 
 
-#REFRESH_LOGO = os.path.abspath(os.path.join(os.path.dirname(__file__), 'glue_refresh.png'))
+REFRESH_LOGO = os.path.abspath(os.path.join(os.path.dirname(__file__), 'glue_refresh.png'))
 
 def setup():
     from .StatsDataViewer import StatsDataViewer
